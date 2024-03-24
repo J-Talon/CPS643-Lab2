565e96fa9c81944694f4a4ec1cbc4d0f6f2c4c8f
J-Talon <cammhd97@gmail.com>
2024-03-24 14:09:00 -0400

    initial commit

---
+++ Assets/Scripts/BalloonActionController.cs
@@ -0,0 +1,123 @@
+﻿
+using UnityEngine;
+
+public class BalloonActionController : MonoBehaviour
+{
+    public GameObject controller;  //extern: the controller
+    
+    private float sizeGain;
+    private float size;
+    private bool deflating;
+    private Rigidbody body;
+    private ConstantForce force;
+    
+    private const float HEIGHT_BOUND = 10;
+    private const float DETACH_SIZE = 3;
+    private const float MIN_SIZE = 0.4f;
+    private const float EXPIRE_TIME = 5;
+
+    private float deflatedTime;
+    
+    void Start()
+    {
+        sizeGain = 0.1f;
+        deflatedTime = 0;
+        size = MIN_SIZE;
+        
+      transform.parent = controller.transform;
+      body = GetComponent<Rigidbody>();
+      force = body.GetComponent<ConstantForce>();
+      deflating = false;
+    }
+
+
+    public void deflate()
+    {
+        deflating = true;
+        setSizeGain(-0.1f);
+    }
+    
+
+   void updateSize()
+    {
+        
+        if (sizeGain == 0)
+            return;
+
+        float gain = Mathf.Lerp(MIN_SIZE, DETACH_SIZE, sizeGain);
+        gain *= Time.deltaTime;
+        
+        if (sizeGain < 0)
+        {
+            if (size < MIN_SIZE)
+                return;
+            
+            size -= gain;
+        }
+        else
+        {
+            if (size >= DETACH_SIZE)
+                return;
+            
+            size += gain;
+        }
+        
+        transform.localScale = new Vector3(size, size, size);
+  
+
+    }
+    
+    void setSizeGain(float newGain)
+    {
+        sizeGain = newGain;
+    }
+    
+    void Update()
+    {
+        updateSize();
+        if (size > DETACH_SIZE)
+        {
+            transform.parent = null;
+            setSizeGain(0);
+        }
+        
+        if (transform.position.y > HEIGHT_BOUND || 
+            transform.position.y < -HEIGHT_BOUND ||
+            deflatedTime >= EXPIRE_TIME)
+        {
+            Destroy(gameObject);
+        }
+
+
+        if (size <= MIN_SIZE)
+        {
+            deflatedTime += Time.deltaTime;
+        }
+    }
+
+
+    void FixedUpdate()
+    {
+        if (!(transform.parent == null))
+            return;
+
+        if (!deflating)
+        {
+            force.enabled = true;
+        }
+        else
+        {
+            
+            if (size <= MIN_SIZE)
+                body.useGravity = true;
+            
+            force.force = new Vector3(0, (-0.3f/size), 0);
+            force.torque = new Vector3(20, 20, 20);
+        }
+    }
+    
+    
+    
+    
+    
+}
---
+++ Assets/Scripts/ControllerInputAction.cs
@@ -0,0 +1,92 @@
+﻿
+using UnityEngine;
+
+
+
+public class ControllerInputAction : MonoBehaviour
+{
+    //extern
+    public GameObject balloonPrefab;
+    public GameObject leftController;
+    public GameObject rightController;
+    public LineRenderer laser;
+   
+     
+     
+    private bool fireLaser;
+    private static GameObject activeBalloon = null;
+    private const float TRACE_DISTANCE = 30;
+
+
+    void Start()
+    {
+        fireLaser = false;
+    }
+    
+    void Update()
+    {
+        if (!fireLaser)
+        {
+            return;
+        }
+
+        
+        Transform form = rightController.transform;
+        Vector3 direction = form.forward;
+        Vector3 startPos = form.position;
+        Ray ray = new Ray(startPos, direction);
+
+        RaycastHit hit;
+        Vector3[] positions;
+        if (Physics.Raycast(ray, out hit, TRACE_DISTANCE))
+        {
+            positions = new Vector3[] { startPos, hit.point };
+            GameObject hitObject = hit.collider.gameObject;
+            if (hitObject.Equals(activeBalloon))
+            {
+                hitObject.GetComponent<BalloonActionController>().deflate();
+            }
+        }
+        else
+        {
+            Vector3 directionDist = new Vector3(direction.x * TRACE_DISTANCE, direction.y * TRACE_DISTANCE,
+                direction.z * TRACE_DISTANCE);
+
+            Vector3 end = new Vector3(startPos.x + directionDist.x, startPos.y + directionDist.y,
+                startPos.z + directionDist.z);
+            
+            positions = new Vector3[] { startPos, end};
+
+        }
+        laser.SetPositions(positions);
+    }
+
+    public void onMenuButtonDown()
+    {
+        
+        
+        if (activeBalloon != null)
+        {
+            return;
+        }
+        
+        balloonPrefab.transform.localScale = new Vector3(1, 1, 1);
+        activeBalloon = Instantiate(balloonPrefab, leftController.transform.position, Quaternion.identity);
+        activeBalloon.SetActive(true);
+            
+    }
+
+    public void onRightButtonDown()
+    {
+        fireLaser = true;
+        laser.enabled = true;
+        
+    }
+
+    public void onRightButtonUp()
+    {
+        fireLaser = false;
+        laser.enabled = false;
+    }
+    
+}
